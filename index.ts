13d2425bc0009a328df4f17845971706fcadf9b5
Amber <amber@transcental.dev>
2025-03-30 03:16:06 +0100

    fix dj orph

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import Slack from "@slack/bolt";
+import Slack, { LogLevel } from "@slack/bolt";
 import Spotify from "spotify-web-api-node";
 import "dotenv/config";
 
@@ -12,7 +12,8 @@ const PLATFORM_MAPPINGS = {
 const app = new Slack.App({
   appToken: process.env.SLACK_APP_TOKEN,
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
 });
 
 const spotify = new Spotify({
@@ -227,5 +228,5 @@ app.event("link_shared", async (ctx) => {
 (async () => {
   await app.start(process.env.PORT ?? 3000);
 
-  console.log(`⚡️ Bolt app is running on port ${process.env.PORT}!`);
+  console.log(`⚡️ Bolt app is running on port ${process.env.PORT || 3000}!`);
 })();
